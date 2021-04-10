58fda7a3b1a09c06e7ea914f7d3c9fa5ee8678c5
Dan Bartlett <danbart909@gmail.com>
2021-04-10 18:01:16 -0400

    prep for first deployment

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 // }
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
