2ea65e54d1e43b3dc8700f516d38ed91d3ecaf49
Dan Bartlett <danbart909@gmail.com>
2021-04-11 17:57:19 -0400

    reverted last change

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://eejredfqabpwhr:f6ba0c7b478b7da22aaee1eba718da41a3641a2de9ccd27d13e72cdf2e1435c5@ec2-52-1-115-6.compute-1.amazonaws.com:5432/dcli93gmf0vhq7',
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY
 }
