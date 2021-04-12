1157b7dc75d2fd57748626889f070b474cf640d4
Dan Bartlett <danbart909@gmail.com>
2021-04-12 11:46:04 -0400

    .

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
+DATABASE_URL.ssl = { rejectUnauthorized: false }
+
 const DATABASE = knex({
   client: 'pg',
-  connection: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connection: DATABASE_URL
 })
 
 app.set('db', DATABASE)
