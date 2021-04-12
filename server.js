796c2661ac5385cc8ea7d2b3693fc227bd5012a7
Dan Bartlett <danbart909@gmail.com>
2021-04-12 12:18:18 -0400

    .

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
-DATABASE_URL.ssl = { rejectUnauthorized: false }
-
 const DATABASE = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  }
 })
 
 app.set('db', DATABASE)
