2007ba837f5f7bf753d9f2d6290571bc35346152
Dan Bartlett <danbart909@gmail.com>
2021-04-13 10:26:15 -0400

    .

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 
 module.exports = {
   PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DATABASE_URL: process.env.DATABASE_URL
   // JWT_SECRET: process.env.JWT_SECRET,
   // JWT_EXPIRY: process.env.JWT_EXPIRY
