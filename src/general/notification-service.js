82b34f53fa03bc4b784f6d1bcc41152d23dab3e7
Dan Bartlett <danbart909@gmail.com>
2021-07-20 16:56:57 -0400

    .

--- src/general/notification-service.js
+++ src/general/notification-service.js
@@ -20,11 +20,11 @@ const notificationService = {
       for (let chunk of chunks) {
         try {
           let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-          console.log(ticketChunk);
+          console.log('Expo - ticketChunk', ticketChunk);
           tickets.push(...ticketChunk);
           // NOTE: If a ticket contains an error code in ticket.details.error, you must handle it appropriately. The error codes are listed in the Expo documentation: https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
         } catch (error) {
-          console.error(error);
+          console.error('Expo - error @ notifications', error);
         }
       }
     })();
@@ -39,17 +39,17 @@ const notificationService = {
 
     let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
     (async () => {
-      // Like sending notifications, there are different strategies you could use
-      // to retrieve batches of receipts from the Expo service.
+      // Like sending notifications, there are different strategies you could use to retrieve batches of receipts from the Expo service.
       for (let chunk of receiptIdChunks) {
         try {
           let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-          console.log(receipts);
+          console.log('Expo - receipts', receipts);
 
           // The receipts specify whether Apple or Google successfully received the
           // notification and information about an error, if one occurred.
           for (let receiptId in receipts) {
             let { status, message, details } = receipts[receiptId];
+            console.log(status, message, details)
             if (status === 'ok') {
               continue;
             } else if (status === 'error') {
@@ -65,7 +65,7 @@ const notificationService = {
             }
           }
         } catch (error) {
-          console.error(error);
+          console.error('notification error', error);
         }
       }
     })();
